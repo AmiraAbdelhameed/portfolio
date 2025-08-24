abfbf5c665da54bba66386b18d60d111604b28bf
Dell <amerakhames42@gmail.com>
2025-08-24 16:41:46 +0300

    Add new projects

--- script.js
+++ script.js
@@ -2,13 +2,27 @@
 // Array of images with corresponding content
 const slides = [
     {
-        img: "/images/night_trip.PNG",
+        img: "./images/night_trip.PNG",
         text: "Night Trips",
         info: 'website for night trips to  discover thrilling night trips, unique destinations, and exciting activities tailored for night owls and adventurers',
         tools: `Created with HTML, CSS, and JavaScript`,
         href:'https://night-trip-git-main-amiras-projects-eba87ddf.vercel.app/'
     },
+  {
+        img: "./images/myCake.png",
+        text: "My Cake Store",
+        info: 'E-commerce website for selling cakes with admin dashboard to add and edit products, receive customers orders and reviews, and manage sales   ',
+        tools: `Created with MUI, Firebase and Reactjs`,
+        href:'https://my-cake-store.vercel.app/'
+    },
     {
+        img: "./images/Ayady.png",
+        text: "Ayady",
+        info: 'Ayady is a platform that connects charities with donors in one place, making donations easy, safe, and clear. We aim to use technology to support charity work and encourage everyone to help make a positive change.',
+        tools: `Created with Reactjs, Redux toolkit , MUI, and supabase ,
+        href:'https://ayady.vercel.app/'
+    },
+       {
         img: "/images/great_outdoors.PNG",
         text: "Great outdoors",
         info: 'website for great outdoors to  discover thrilling great and different places, unique destinations, and exciting activities tailored for great outdoors',
